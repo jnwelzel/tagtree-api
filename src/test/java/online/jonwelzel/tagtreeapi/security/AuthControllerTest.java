8a5ff0c8d96c1b17c2a6a71736cbdf44b99f43d7
Jonathan Welzel <jnwelzel@gmail.com>
2024-10-04 11:29:56 +0200

    Added auth controller tests

---
+++ src/test/java/online/jonwelzel/tagtreeapi/security/AuthControllerTest.java
@@ -0,0 +1,41 @@
+package online.jonwelzel.tagtreeapi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void login() throws Exception {
+        this.mockMvc
+                .perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\": \"bbaggins@shire.com\",\"password\": \"admin\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.accessToken").exists());
+    }
+
+    @Test
+    void register() throws Exception {
+        this.mockMvc
+                .perform(post("/api/v1/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\": \"user@test.com\",\"password\": \"password\",\"userName\": \"testUser\",\"firstName\": \"Test\",\"lastName\": \"User\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").exists());
+    }
+}
